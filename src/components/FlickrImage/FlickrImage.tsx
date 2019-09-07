7c6e20aa4dc6b5b7272934ab98df4f1a497eb93b
Gab Labelle <gab@visualise.ca>
2019-09-07 11:14:12 -0400

    Improved Image component, hides broken img links.

--- src/components/FlickrImage/FlickrImage.tsx
+++ src/components/FlickrImage/FlickrImage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, SyntheticEvent } from 'react';
 import styled from 'styled-components';
 import { FlickrPhoto } from '../../providers/FlickrProvider';
 
@@ -6,12 +6,14 @@ interface FlickrImageProps {
   img: FlickrPhoto;
 }
 
+interface FlickrImageState {
+  isVisible: boolean;
+  error: boolean;
+}
+
 const ImageWrapper = styled.div`
-  background-color: white;
-  border-radius: 5px;
-  box-sizing: border-box;
+  height: auto;
   margin: 10px;
-  padding: 10px;
   width: 100%;
 
   @media (min-width: 420px) {
@@ -19,35 +21,70 @@ const ImageWrapper = styled.div`
   }
 
   @media (min-width: 610px) {
+    height: 170px;
     width: 170px;
   }
 
   @media (min-width: 662px) {
+    height: 188px;
     width: 188px;
   }
 
   @media (min-width: 800px) {
+    height: 170px;
     width: 170px;
   }
 `;
 
-const Image = styled.img`
+const Image = styled.img<{ isVisible: boolean; error: boolean }>`
+  background-color: white;
+  border-radius: 5px;
+  box-sizing: border-box;
+  display: ${({ error }) => (error ? 'none' : 'block')};
   height: auto;
+  opacity: ${({ isVisible }) => (isVisible ? 100 : 0)};
+  padding: 10px;
+  transition: opacity 100ms;
   width: 100%;
 `;
 
-function FlickrImage({ img }: FlickrImageProps) {
-  const { ownername, title, url_q } = img;
+class FlickrImage extends Component<FlickrImageProps, FlickrImageState> {
+  state = {
+    isVisible: false,
+    error: false
+  };
 
-  return (
-    <ImageWrapper>
-      <Image
-        src={url_q}
-        alt={`${title} by ${ownername}`}
-        title={`${title} by ${ownername}`}
-      />
-    </ImageWrapper>
-  );
+  handleLoadedImage = () => {
+    this.setState({
+      isVisible: true
+    });
+  };
+
+  handleImageError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    this.setState({
+      error: true
+    });
+    console.error(`Could not load image:`, e.currentTarget);
+  };
+
+  render() {
+    const { ownername, title, url_q } = this.props.img;
+    const { isVisible, error } = this.state;
+
+    return (
+      <ImageWrapper>
+        <Image
+          src={url_q}
+          alt={`${title} by ${ownername}`}
+          title={`${title} by ${ownername}`}
+          onLoad={this.handleLoadedImage}
+          onError={this.handleImageError}
+          isVisible={isVisible}
+          error={error}
+        />
+      </ImageWrapper>
+    );
+  }
 }
 
 export default FlickrImage;
